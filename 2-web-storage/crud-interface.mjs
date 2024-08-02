bbaf3d5b35b9664c5cfe7c5198efffa7e149d0b9
christianzhou1 <christianzhiruizhou@outlook.com>
2024-08-02 14:23:42 -0400

    add part 2 web storage

---
+++ 2-web-storage/crud-interface.mjs
@@ -0,0 +1,40 @@
+export default function (toDoList = []) {
+  console.log("Function called with toDoList:", toDoList);
+  var index = toDoList.length + 1;
+  return {
+    createToDo,
+    readToDoList,
+    updateToDo,
+    deleteToDo,
+    deleteToDo2,
+  };
+  function createToDo(toDoText) {
+    toDoList.push({
+      text: toDoText,
+      id: `${index++}`,
+      done: false,
+    });
+    console.log("ToDo list after adding:", toDoList);
+  }
+  function readToDoList() {
+    return structuredClone(toDoList);
+  }
+  function updateToDo(toDoId) {
+    var toDo = toDoList.find((toDo) => toDo.id === toDoId);
+    if (toDo) {
+      toDo.done = !toDo.done;
+    }
+    console.log("ToDo list after updating:", toDoList);
+  }
+  function deleteToDo(toDoId) {
+    const toDoItemIndex = toDoList.findIndex((toDo) => toDo.id === toDoId);
+    if (toDoItemIndex > -1) {
+      toDoList.splice(toDoItemIndex, 1);
+    }
+    console.log("ToDo list after deleting:", toDoList);
+  }
+  function deleteToDo2(toDoId) {
+    toDoList = toDoList.filter((toDo) => toDo.id != toDoId);
+    console.log("ToDo list after deleting (2):", toDoList);
+  }
+}
