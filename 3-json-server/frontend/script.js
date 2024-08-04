5c040eda7a44bfba3eb67816fadf859009d4a5bf
christianzhou1 <christianzhiruizhou@outlook.com>
2024-08-04 19:18:01 -0400

    add front end

--- 3-json-server/frontend/script.js
+++ 3-json-server/frontend/script.js
@@ -1,14 +1,47 @@
 import CrudInterface from "./crud-interface.mjs";
+const crudInterface = new CrudInterface();
+console.log("fetch data from json-server:", await crudInterface.readToDoList());
 
-async function main() {
-  fetch("http://localhost:3000/toDos")
-    .then((response) => response.json())
-    .then((response) => console.log(response));
-
-  const crudInterface = new CrudInterface();
-  console.log(
-    "fetch data from json-server:",
-    await crudInterface.readToDoList()
-  );
+async function getToDoList() {
+  const toDos = await crudInterface.readToDoList();
+  console.log("readToDoList", toDos);
+  showToDoList(toDos);
 }
-main();
+
+const showToDoItem = (toDo) =>
+  toDoItem.innerHTML
+    .replace("toDoAction", toDo.done ? "Del" : "Done")
+    .replace("{toDoTitle}", toDo.text)
+    .replaceAll("{toDoId}", toDo.id);
+const showToDoList = (toDos) =>
+  (toDoList.innerHTML = toDos.map(showToDoItem).join(""));
+
+txtToDoTitle.addEventListener("input", (evt) => {
+  btnAddToDo.disabled = !evt.target.value;
+});
+
+btnAddToDo.disabled = true;
+
+// const input = document.querySelector("input");
+// const addButton = document.querySelector("add");
+
+toDoForm.addEventListener("click", async (evt) => {
+  if (evt.target.nodeName === "BUTTON") {
+    if (evt.target.textContent === "Add") {
+      await crudInterface.createToDo(txtToDoTitle.value);
+      console.log("added todo item", txtToDoTitle.value);
+      txtToDoTitle.value = "";
+    }
+    if (evt.target.textContent === "Done") {
+      await crudInterface.updateToDo(evt.target.dataset.todo);
+      console.log("updated todo item", evt.target.dataset.todo);
+    }
+    if (evt.target.textContent === "Del") {
+      await crudInterface.deleteToDo(evt.target.dataset.todo);
+      console.log("Deleted todo item", evt.target.dataset.todo);
+    }
+  }
+  getToDoList();
+});
+
+getToDoList();
