9789adb31a691cc8a33bbb2122f770fadf32b920
christianzhou1 <christianzhiruizhou@outlook.com>
2024-08-04 20:25:46 -0400

    initialize part 4 - express array

---
+++ 4-express-array/backend/server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const CrudInterface = require('./crud-interface.js');
+const testData = require('../db.json');
